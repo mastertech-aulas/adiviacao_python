50cae0b0b44ab44bdcb88bfd0d30ecff7233ff81
Vinicius Ferreira <vferreira@mastertech.tech>
2019-07-29 10:05:47 -0300

    Adicionar resolucao para Fase 2

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,16 +1,29 @@
 import random
+from enum import Enum
 
-numero_aleatorio = random.randint(0, 50)
+class Dificuldade(Enum):
+    FACIL = 10
+    NORMAL = 50
+    DIFICIL = 100
+
+nivel_dificuldade = Dificuldade[input("Qual a dificuldade? \nFACIL \nMedio \nDIFICIL\n:").upper()].value
+
+numero_aleatorio = random.randint(0, nivel_dificuldade)
 tentativas = 5
+
+chutes = []
 
 while(tentativas != 0):
 
     chute = int(input('Digite seu chute:\n'))
-
-    if chute == numero_aleatorio:
-        print('\nParabéns você ganhou')
-        tentativas = 0
+    
+    if chute not in chutes:    
+        if chute == numero_aleatorio:
+            print('\nParabéns você ganhou')
+            tentativas = 0
+        else:
+            chutes.append(chute)
+            tentativas -= 1
+            print('\nVocê errou, mas ainda tem '+str(tentativas)+' Tentativas')
     else:
-        tentativas -= 1
-        print('\nVocê errou, mas ainda tem '+str(tentativas)+' Tentativas')
-        
+        print('Você já tentou esse numero. Tente com outro numero')
